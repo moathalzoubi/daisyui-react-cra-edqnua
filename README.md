17fb2ed407c03a53f9262bf09298c865e49c38ab
moathalzoubi <32450805+moathalzoubi@users.noreply.github.com>
2022-12-11 09:16:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-cra-edqnua
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-cra-cuagr1)
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppBar } from './Nav';
+
+const App = (props) => {
+  return (
+    <div>
+      <AppBar />
+    </div>
+  );
+};
+export default App;
